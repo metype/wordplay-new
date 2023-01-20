a64f9440a7f41a698be61b8d38ba18595a4845c3
Metype <39036543+metype@users.noreply.github.com>
2023-01-19 20:38:33 -0600

    Set up Vuetify

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <v-app class="app">
+    
+  </v-app>
+</template>
+
+<style scoped>
+body {
+  overflow: hidden;
+}
+
+.app {
+  width:100vw;
+  height:100vh;
+  margin-left:0;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import vuetify from './plugins/vuetify'
+
+
+createApp(App).use(vuetify).mount('#app')
